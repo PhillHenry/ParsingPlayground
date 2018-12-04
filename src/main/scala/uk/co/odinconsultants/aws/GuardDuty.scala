8f85356a344b2f98d16f969aee7430c44d773d76
PhillHenry <phillhenry@gmail.com>
2018-12-04 09:44:20 +0000

    Formatting.

--- src/main/scala/uk/co/odinconsultants/aws/GuardDuty.scala
+++ src/main/scala/uk/co/odinconsultants/aws/GuardDuty.scala
@@ -6,18 +6,18 @@ object GuardDuty {
 
   case class Detail(connection: Connection, createdAt: Date)
 
-  case class Connection(direction: String,
-                        ipAddressV4: String,
-                        org: String,
-                        asnOrg: String,
-                        isp: String,
-                        countryName: String,
-                        cityName: String,
-                        lat: Double,
-                        lon: Double,
-                        protocol: String,
-                        blocked: Boolean,
-                        remote: Int,
-                        local: Int)
+  case class Connection(direction:    String,
+                        ipAddressV4:  String,
+                        org:          String,
+                        asnOrg:       String,
+                        isp:          String,
+                        countryName:  String,
+                        cityName:     String,
+                        lat:          Double,
+                        lon:          Double,
+                        protocol:     String,
+                        blocked:      Boolean,
+                        remote:       Int,
+                        local:        Int)
 
 }
